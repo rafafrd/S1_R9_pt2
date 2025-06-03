15bf6aab286c32fb7c0d2f81f66d6df6041d75d9
rafafrd <bolekao03@gmail.com>
2025-06-03 12:58:00 -0300

    começando

--- 6.js
+++ 6.js
@@ -1,3 +1,23 @@
 // Desenvolva um programa que colete e armazene os nomes e as alturas de diferentes pessoas em um vetor. 
 // O programa deve permitir que o usuário insira quantas alturas desejar e só deve ser encerrado quando o 
 // usuário optar por parar. Ao final, exiba a maior altura, a menor altura e a quem pertencem respectivamente.
+let nomes = [];
+let alturas = [];
+
+let continuar = true;
+let contador = 0;
+while (continuar == true) {
+  let nome = prompt("Digite o nome da pessoa: ")
+  let altura = parseFloat(prompt("Digite a altura: "))
+
+  nomes[contador] = nome
+  alturas[contador] = altura
+  contador++
+
+  let resposta = prompt("Deseja inserir outra pessoa (S/N)")
+  if (resposta == "n" || resposta == "N" || resposta === "não" || resposta === "nao"|| resposta === "Não" || resposta === "Nao") {
+    continuar = false
+  }
+}
+console.log(nomes)
+console.log(alturas)
